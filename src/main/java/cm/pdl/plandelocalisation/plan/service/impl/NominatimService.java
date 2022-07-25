7c55326511ba8790b914bc68d47712b330b2a3d8
Georges DEFO <defo.georges@gmail.com>
2022-07-25 20:21:58 +0200

    ✨ template enhancement
    Code cleaning, QR code creation, Identifier creation

---
+++ src/main/java/cm/pdl/plandelocalisation/plan/service/impl/NominatimService.java
@@ -0,0 +1,36 @@
+package cm.pdl.plandelocalisation.plan.service.impl;
+
+import cm.pdl.plandelocalisation.plan.service.LocationInformationService;
+import cm.pdl.plandelocalisation.plan.dto.LocationDTO;
+import cm.pdl.plandelocalisation.plan.dto.PlaceDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * @author Georges DEFO
+ * @date 25/07/2022
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NominatimService implements LocationInformationService {
+
+    private final WebClient webClient;
+
+    @Override
+    public PlaceDTO getInformation(LocationDTO locationDTO) {
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/reverse")
+                        .queryParam("lat", locationDTO.getLatitude())
+                        .queryParam("lon", locationDTO.getLongitude())
+                        .queryParam("format", "jsonv2")
+                        .build())
+                .retrieve()
+                .bodyToMono(PlaceDTO.class)
+                .block();
+    }
+}
